fbb9d496d8d9ffa75414a649aa132dd603b9b70c
mcglauflins <sarahriebschlager@gmail.com>
2022-03-22 01:03:18 +0000

    commits

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function genDomain() {
+  let domainName = [];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          domainName.push(" " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
+
+  const genDomain = () => {
+    document.getElementById("domain.results").innerHTML = genDomain();
+  };
+}
+document.getElementById("myBtn").addEventListener("click".setRandomName);
+
+setRandomName();
